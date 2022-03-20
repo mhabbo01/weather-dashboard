1288ee284901703aeb39db829cc354c167298ba4
Matt Habbo <matthabbo45@gmail.com>
2022-03-20 12:09:23 -0400

    created functionality for weather, current day and five day weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,169 @@
-//set global query selectors
-
-let cities = [];
-let searchFormEl = document.querySelector("#search-form");
-let cityInputEl = document.querySelector("#city");
-let pastSearchEl = document.querySelector("#past-search");
-let currentWeatherEl = document.querySelector("#current-weather");
-let searchedCityEl = document.querySelector("#searched-city");
-let forcastEl = document.querySelector("#forecast");
-let fiveDayEl = document.querySelector("#five-day-forecast");
+// key words from index
+// id past-search for past city searches
+// class btnDiv for div parent of class city
+// class city for <input>
+//class fiveDay for five day forecast
+// id currentCity
+//id currentDate
 
 
-let formSubmitHandler = function(event) {
-    event.preventDefault();  //prevents the browser from refreshing
-    let city = cityInputEl.value.trim();
-    if (city) {
-        cityWeather(city);
+// let apiKey = "b6f5efc6e0c2b9553c818aa502e235f3";
+let citySearch = document.querySelector(".current-city");
+let fiveDay = $("#fiveDay");
+let todayBody = $("#todayBody");
+
+let date = moment().format("LL");
+
+let searchHistory = [];
+
+$(".search-btn").on("click", function (event){
+    event.preventDefault();
+    let city = $("#city").val().trim();
+    console.log(city)
+    // set a loop to append the search history under the search bar
+    if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+        let searchedCity = $(`
+        <li class="list-group-item bg-dark text-light">${city}</li>`);
+        $("#searchHistory").append(searchedCity);
+    };
+
+    localStorage.setItem("city", JSON.stringify(searchHistory));
+
+    console.log("click");
+    getWeather(city);
+});
+
+// search history items to be clickable
+$(document).on("click", ".list-group-item", function() {
+    let lastCity = $(this).text();
+    getWeather(lastCity);
+});
+
+// sets the last searched city when the page is reloaded
+$(document).ready(function() {
+    let searchHisArr = JSON.parse(localStorage.getItem("city"));
+    if (searchHisArr !== null) {
+        let searchHisIndex = searchHisArr.length - 1;
+        let lastSearchedCity = searchHisArr[searchHisIndex];
+        getWeather(lastSearchedCity);
 
     }
-}
+});
 
-// input the API key & url and fetch the data for the city that is searched
-let cityWeather = function(city) {
-    let appid = "b6f5efc6e0c2b9553c818aa502e235f3"
-    let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&" + appid
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) { //json displays the data in a readable format
-            displayWeather(data, city);
+
+
+function getWeather(city) {
+    let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=b6f5efc6e0c2b9553c818aa502e235f3`;
+    todayBody.empty(); //empties out the today info from the last search
+
+    fetch(queryUrl)
+        .then(function (response) {
+            return response.json(); // returns the data in a readable form
+        })    
+        .then(function (response) {
+            // console.log(response);
+            $("#currentCity").text(response.name);
+            $("#currentDate").text(date);
+            //grabbing the icons from the response
+            // let icon = response.weather[0].icon;
+            let weatherIcon = document.createElement("img")
+            weatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+            citySearch.appendChild(weatherIcon);
+            //creating elements for the icons and appending
+            let tempEl = document.createElement("p"); //create a <p> element and append it to todayBody
+            tempEl.textContent = "Temperature: " + response.main.temp + " °F";
+            todayBody.append(tempEl);
+            let humidEl = document.createElement("p");
+            humidEl.textContent = "Humidity: " + response.main.humidity + " %";
+            todayBody.append(humidEl);
+            let windEl = document.createElement("p");
+            windEl.textContent = "Wind Speed: " + response.wind.speed + " MPH";
+            todayBody.append(windEl);
+
+            let cityLat = response.coord.lat;
+            let cityLon = response.coord.lon;
+            console.log(cityLat)
+
+            getFiveDay(cityLat, cityLon);
+
+            let uviUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=b6f5efc6e0c2b9553c818aa502e235f3`;
+
+            fetch(uviUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    console.log(response);
+                    let uviPEl = document.createElement("p");
+                    uviPEl.textContent = ("UV Index: ");
+                    todayBody.append(uviPEl);
+                    let todayUvi = document.createElement("span");
+                    todayUvi.textContent = response.current.uvi;
+                    let uviEl = response.current.uvi;
+                    uviPEl.append(todayUvi);
+
+
+                    if (uviEl >= 0 && uviEl <= 2) {
+                        todayUvi.setAttribute("style", "background-color: green");
+                    } else if (uviEl > 2 && uviEl <= 5) {
+                        todayUvi.setAttribute("style", "background-color: yellow");
+                    } else if (uviEl > 5 && uviEl <= 7) {
+                        todayUvi.setAttribute("style", "background-color: orange");
+                    } else if (uviEl > 7 && uviEl <= 10) {
+                        todayUvi.setAttribute("style", "background-color: red");
+                    } else todayUvi.setAttribute("style", "background-color: purple");
+                });
+
+
         });
-    });
+        
+    
 };
 
-let displayWeather = function(weather, searchCity) {
-    currentWeatherEl.textContent = "";
-    cityInputEl.textContent = "";
+function getFiveDay(cityLat, cityLon) {
+    let fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=b6f5efc6e0c2b9553c818aa502e235f3`
 
-    console.log(weather);
-}
+            fetch(fiveDayUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    fiveDay.empty(); //empties out the five day from the last search
+                    //get info for 5 days and loop
+                    for (let i = 1; i < 6; i++) {
+                        //store info into an object to make things easier
+                        let fiveDayInfo = {
+                            date: response.daily[i].dt,
+                            icon: response.daily[i].weather[0].icon,
+                            temp: response.daily[i].temp.day,
+                            wind: response.daily[i].wind_speed,
+                            humid: response.daily[i].humidity
+
+                        };
+                        // set date for each day
+                        let currentDate = moment.unix(fiveDayInfo.date).format("LL");
+                        // set the url for the icons that will be used for each day
+                        let weatherIcon = `<img src="https://openweathermap.org/img/w/${fiveDayInfo.icon}.png" alt="${response.daily[i].weather[0].main}" />`
+                        
+                        // create lines of HTML to be appended under fiveDay
+                        let fiveDayCard = $(`
+                        <div class="col pl-3">
+                            <div class="card pl-3 pt-3 mb-3 bg-dark text-light" style="width: 12rem">
+                                <div class="card-body">
+                                    <h5>${currentDate}</h5>
+                                    <p>${weatherIcon}</p>
+                                    <p>Temp: ${fiveDayInfo.temp} °F</p>
+                                    <p>Humidity: ${fiveDayInfo.humid}\%</p>
+                                    <p>Wind Speed: ${fiveDayInfo.wind} MPH</p>
+                                </div>
+                            </div>
+                        <div>
+                    `);
+
+                        fiveDay.append(fiveDayCard);
+
+                    };
+
+                });
+};
